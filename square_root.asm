d27b3f59e05e501591268de96f108979ac2653c5
Zakariaa <88563786+lowlevel01@users.noreply.github.com>
2025-05-21 12:37:03 +0100

    Create square_root.asm

---
+++ square_root.asm
@@ -0,0 +1,30 @@
+section .text
+global square_root
+square_root:
+    cmp rdi, 0
+    mov rcx, 0
+    jz .return
+    mov rcx, 1
+.loop:
+    mov rax, rcx
+    mul rcx
+
+    cmp rax, rdi
+    jz .return ;;; they're equal
+    jg .more ;;; rax > rdi
+.less:
+    mov rbx, rcx
+    shl rcx, 1 ;; rcx = 2*rcx
+    jmp .loop
+.more:
+    sub rcx, rbx
+    shr rcx, 1  ;;;;  rcx = rbx + (rcx-rbx) / 2
+    add rcx, rbx
+    jmp .loop
+.return:
+    mov rax, rcx
+    ret
+
+%ifidn __OUTPUT_FORMAT__,elf64
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
